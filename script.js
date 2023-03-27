0a5a3b4f699f42ef1e3c5b9635b19fb9b03e62e7
Bala Chandra Akula <115984987+BalaChandraAkula@users.noreply.github.com>
2023-03-27 21:35:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 const url = "https://jsonplaceholder.typicode.com/posts/1";
 
 //your JS code here. If required.
+const app = require("./index.js");
+app.listen(3000, () => {
+  console.log('server started');
+});
